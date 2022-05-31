f7a825cb62af940ed6df8d6cb2339eba0ed83e02
jon@it-minds.dk <jon@it-minds.dk>
2022-05-31 23:38:23 +0200

    added modal for registering a new item

--- frontend/src/pages/gallery/index.tsx
+++ frontend/src/pages/gallery/index.tsx
@@ -1,3 +1,14 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from "@chakra-ui/react";
 import GalleryItem from "components/LagerService/GalleryItem";
 import useItemContext from "contexts/useItemContext";
 import { NextPage } from "next";
@@ -5,6 +16,7 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 const IndexPage: NextPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { items, fetchItems } = useItemContext();
 
   useRouter();
@@ -20,6 +32,21 @@ const IndexPage: NextPage = () => {
     <div>
       <p>lol</p>
       <GalleryItem storage={items} />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Register new item</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody></ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Button onClick={onOpen}>Open Modal</Button>
     </div>
   );
 };
