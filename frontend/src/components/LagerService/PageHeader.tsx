ea2ca04b9544a83ed236aa52ad5cecd939044936
jon@it-minds.dk <jon@it-minds.dk>
2022-06-08 17:08:29 +0200

    Frontpage

--- frontend/src/components/LagerService/PageHeader.tsx
+++ frontend/src/components/LagerService/PageHeader.tsx
@@ -1,6 +1,6 @@
 import { Heading, Stack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 
 type Props = {
   sizeMultiplier?: number;
@@ -9,32 +9,31 @@ type Props = {
 
 const HEADERSIZE = {
   1: "lg",
-  2: "xl"
+  2: "xl",
+  3: "2xl",
+  4: "3xl",
+  5: "4xl",
+  10: "9xl"
 };
 
 const PageHeader: FC<Props> = ({ sizeMultiplier = 1, invert }) => {
-  const [showButton, setShowButton] = useState<boolean>(true);
-
   const router = useRouter();
-  const path = router.asPath;
 
   return (
     <React.Fragment>
-      <Stack direction="row" my="3rem">
+      <Stack direction="row">
         <Stack flex={1}></Stack>
         <Stack flex={4} direction="row" justify="center" align="center">
           <Heading
-            color={invert ? "White" : "Black"}
-            size={HEADERSIZE[sizeMultiplier]}
+            color={invert ? "whitesmoke" : "Black"}
+            fontSize={HEADERSIZE[sizeMultiplier]}
             cursor="pointer"
             onClick={() => router.push("/")}>
             Easy storage
           </Heading>
         </Stack>
-
         <Stack flex={1}></Stack>
       </Stack>
-      {/* <Box height={"2px"} width="auto" borderWidth={"1px"} /> */}
     </React.Fragment>
   );
 };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,64 @@
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Stack,
+  VStack
+} from "@chakra-ui/react";
+import LineBreak from "components/Common/LineBreak";
+import PageHeader from "components/LagerService/PageHeader";
 import { Locale } from "i18n/Locale";
 import { GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import { I18nProps } from "next-rosetta";
-import React, { useEffect } from "react";
+import React, { useCallback } from "react";
 
 const IndexPage: NextPage = () => {
   const router = useRouter();
 
-  useEffect(() => {
+  const Login = useCallback(() => {
     router.push(`/gallery/`);
   }, [router]);
 
-  return <p>Hello World!</p>;
+  //TODO: BackGrounds for input fields must be white when focused, and some other meaningful color when not
+
+  return (
+    <Container
+      height="100vh"
+      padding={"10rem"}
+      maxW="1x1"
+      bgGradient="linear(to-t, blue.400, blue.600)">
+      <PageHeader sizeMultiplier={10} invert />
+      <HStack justify={"center"} height="70vh">
+        <VStack width="25vw" align={"flex-start"}>
+          <Stack flex={2} />
+          <Stack flex={4} width="100%">
+            <FormControl color={"whitesmoke"} fontSize="lg">
+              <FormLabel color={"whitesmoke"} fontSize="lg">
+                Email
+              </FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <LineBreak breaks={2} />
+            <FormControl color={"whitesmoke"} fontSize="lg">
+              <FormLabel color={"whitesmoke"} fontSize="lg">
+                Password
+              </FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <LineBreak />
+            <Button onClick={Login} type="submit" colorScheme="teal">
+              Login
+            </Button>
+          </Stack>
+          <Stack flex={1} />
+        </VStack>
+      </HStack>
+    </Container>
+  );
 };
 
 export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context => {
