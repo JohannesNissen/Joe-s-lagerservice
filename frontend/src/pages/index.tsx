b402c6a7ed520c683eeb4b8722cdb398fba7f44e
jon@it-minds.dk <jon@it-minds.dk>
2022-06-08 11:39:08 +0200

    Imidiate redirect

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,16 @@
 import { Locale } from "i18n/Locale";
 import { GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
 import { I18nProps } from "next-rosetta";
-import React from "react";
-import { withAuth } from "services/auth/withAuth";
+import React, { useEffect } from "react";
 
 const IndexPage: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(`/gallery/`);
+  }, [router]);
+
   return <p>Hello World!</p>;
 };
 
@@ -17,4 +23,5 @@ export const getStaticProps: GetStaticProps<I18nProps<Locale>> = async context =
   };
 };
 
-export default withAuth(IndexPage, { authSkip: true });
+export default IndexPage;
+// export default withAuth(IndexPage, { authSkip: true });
