ae0ef3df299417f9ad9c7114e0152ede3bc14c1a
jon@it-minds.dk <jon@it-minds.dk>
2022-06-13 10:35:12 +0200

    Added som conditional formatting for the header

--- frontend/src/components/LagerService/PageHeader.tsx
+++ frontend/src/components/LagerService/PageHeader.tsx
@@ -1,9 +1,10 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import { Avatar, Heading, Stack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React, { FC, useState } from "react";
 
 type Props = {
   sizeMultiplier?: number;
+  frontPage?: boolean;
   invert?: boolean;
 };
 
@@ -12,17 +13,40 @@ const HEADERSIZE = {
   2: "xl"
 };
 
-const PageHeader: FC<Props> = ({ sizeMultiplier = 1, invert }) => {
+const PageHeader: FC<Props> = ({ sizeMultiplier = 1, invert, frontPage }) => {
   const [showButton, setShowButton] = useState<boolean>(true);
 
   const router = useRouter();
   const path = router.asPath;
 
-  return (
-    <React.Fragment>
-      <Stack direction="row" my="3rem">
-        <Stack flex={1}></Stack>
-        <Stack flex={4} direction="row" justify="center" align="center">
+  if (!frontPage)
+    return (
+      <React.Fragment>
+        <Stack direction="row" mt="1rem" mb="3rem" border="2px black">
+          <Stack flex={1} direction="row" justify="center" align="center">
+            <Heading
+              color={invert ? "White" : "Black"}
+              size={HEADERSIZE[sizeMultiplier]}
+              cursor="pointer"
+              onClick={() => router.push("/")}>
+              Easy storage
+            </Heading>
+          </Stack>
+          <Stack flex={4} direction="row" justify="flex-start" align="center"></Stack>
+
+          <Stack flex={1}>
+            <Avatar name="Test User" />
+          </Stack>
+        </Stack>
+        {/* <Box height={"2px"} width="auto" borderWidth={"1px"} /> */}
+      </React.Fragment>
+    );
+  else
+    return (
+      <React.Fragment>
+        <Stack direction="row" my="3rem"></Stack>
+        <Stack flex={4} direction="row" justify="flex-start" align="center"></Stack>
+        <Stack flex={1} direction="row" justify="center" align="center">
           <Heading
             color={invert ? "White" : "Black"}
             size={HEADERSIZE[sizeMultiplier]}
@@ -30,13 +54,13 @@ const PageHeader: FC<Props> = ({ sizeMultiplier = 1, invert }) => {
             onClick={() => router.push("/")}>
             Easy storage
           </Heading>
+          <Stack flex={1}>
+            <Avatar name="Test User" />
+          </Stack>
         </Stack>
-
-        <Stack flex={1}></Stack>
-      </Stack>
-      {/* <Box height={"2px"} width="auto" borderWidth={"1px"} /> */}
-    </React.Fragment>
-  );
+        {/* <Box height={"2px"} width="auto" borderWidth={"1px"} /> */}
+      </React.Fragment>
+    );
 };
 
 export default PageHeader;
