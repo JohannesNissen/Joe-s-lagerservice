c204b7da4bc269ffbcb95a7b901413bb33ab4574
jon@it-minds.dk <jon@it-minds.dk>
2022-07-15 15:11:19 +0200

    added contentId for notifocations

---
+++ backend/Infrastructure/Persistence/Migrations/20220712184734_Notification_content_id.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class Notification_content_id : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ContentId",
+                table: "Notifications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentId",
+                table: "Notifications");
+        }
+    }
+}
